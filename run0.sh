fae2de0b8cfaa0c291c8312ce29ff84378fec966
Archil Bolkvadze <a.bolkvadze@gmail.com>
2014-11-25 12:57:04 +0400

    renames

--- run0.sh
+++ run0.sh
@@ -3,23 +3,23 @@ rm -rf out
 mkdir out
 
 mkdir out/baratebi
-echo ferma2.xlsx | ./mapreduce.js --outDir out/baratebi --mrscript ./mr00.js > /dev/null
+echo ferma2.xlsx | ./mapreduce.js --outDir out/baratebi --mrscript ./mr-baratebi.js > /dev/null
 
 mkdir out/genetika
-find out/baratebi -type f | ./mapreduce.js --outDir out/genetika --mrscript ./mr01.js > /dev/null
+find out/baratebi -type f | ./mapreduce.js --outDir out/genetika --mrscript ./mr-genetika.js > /dev/null
 
-find out/genetika -type f | ./mapreduce.js --outDir ../ferma-web --mrscript ./mr02.js > /dev/null
+find out/genetika -type f | ./mapreduce.js --outDir ../ferma-web --mrscript ./mr-ferma-web.js > /dev/null
 
 mkdir out/mdgomareoba
-find out/baratebi -type f | ./mapreduce.js --outDir out/mdgomareoba --mrscript ./mrfuri.js > /dev/null
+find out/baratebi -type f | ./mapreduce.js --outDir out/mdgomareoba --mrscript ./mr-mdgomareoba.js > /dev/null
 
 mkdir out/raodenobebi
-find out/baratebi -type f | ./mapreduce.js --outDir out/raodenobebi --mrscript ./mrraodenobebi.js > /dev/null
+find out/baratebi -type f | ./mapreduce.js --outDir out/raodenobebi --mrscript ./mr-raodenobebi.js > /dev/null
 
 mkdir out/report
-find out/ -type f | ./mapreduce.js --outDir out/report --mrscript ./mrmerge.js > /dev/null
+find out/ -type f | ./mapreduce.js --outDir out/report --mrscript ./mr-report.js > /dev/null
 
 mkdir out/excel-report
-find out/report -type f | ./mapreduce.js --outDir out/excel-report --mrscript ./mrexcel.js > /dev/null
+find out/report -type f | ./mapreduce.js --outDir out/excel-report --mrscript ./mr-excel-report.js > /dev/null
 
 ./json2xlsx.js ./out/excel-report/array.json ./rep.xlsx
--- run2.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/bash
-rm -rf out
-mkdir out
-echo ferma2.xlsx | ./mapreduce.js --outDir out --mrscript ./mr01.js
-find out -type f -name "*.json" | ./mapreduce.js --outDir ../ferma-web --mrscript ./mr02.js
